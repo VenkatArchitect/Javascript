7707912910cb1cfb0862a695e05457c397479421
Venkatasubramaniam (Venkat) Ramakrishnan <venkat.architect@gmail.com>
2020-10-29 16:34:11 +0530

    Update lang.js

--- lang.js
+++ lang.js
@@ -2,3 +2,8 @@
 function removeVowels(str) {
   return str.replace(/[aeiou]/g, '')
 }
+
+// count the no. of words in sentences
+function countWords(str) {
+    return(str.split(/[^\s]+/).length - 1) ;	
+}
