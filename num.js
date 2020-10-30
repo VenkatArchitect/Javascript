b85420c129364c3883a0af95cef56c6e2cac7072
Venkatasubramaniam (Venkat) Ramakrishnan <venkat.architect@gmail.com>
2020-10-30 07:28:38 +0530

    Update num.js

--- num.js
+++ num.js
@@ -1,13 +1,18 @@
-function findSmallestNum(arr) {
+function diffMaxMin(arr) {
+    
+  var l_count ;
+  var l_smallest = Number.MAX_VALUE;
+  var l_biggest = Number.MIN_VALUE ;
 
-    var l_count ;
-    var l_smallest = Number.MAX_VALUE;
+  for (l_count = 0; l_count < arr.length; l_count ++) {
+      if (arr[l_count] < l_smallest) {
+          l_smallest = arr[l_count] ;
+      }
 
-    for (l_count = 0; l_count < arr.length; l_count ++) {
-        if (arr[l_count] < l_smallest) {
-            l_smallest = arr[l_count] ;
-        }
-    }
+      if (arr[l_count] > l_biggest) {
+          l_biggest = arr[l_count] ;
+      }
+  }
 
-    return (l_smallest) ;
+  return (l_biggest - l_smallest) ;
 }
