b360951cee8c233876adbb3b2fc0fd6b840af3ef
Venkatasubramaniam (Venkat) Ramakrishnan <venkat.architect@gmail.com>
2020-10-30 07:11:47 +0530

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,9 +1,9 @@
 // This function removes all the vowels in a string
-function removeVowels(str) {
+function js_removeVowels(str) {
   return str.replace(/[aeiou]/g, '')
 }
 
 // count the no. of words in sentences
-function countWords(str) {
+function js_countWords(str) {
     return(str.split(/[^\s]+/).length - 1) ;	
 }
